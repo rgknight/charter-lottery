e8e677425d5df5c628c307e0975558b347bedfcc
rgknight <ryangknight@gmail.com>
2015-03-09 14:40:22 -0400

    Final Draft of UAD lottery file

--- Run Lottery for UP Academy Dorchester.R
+++ Run Lottery for UP Academy Dorchester.R
@@ -21,10 +21,35 @@ raw <- read.csv("UAD 2015-2016 Applicants.csv")
 
 set.seed(43412696) # Change to a random number from random.org on lottery night
 
-# All applications should have an "Applicant_ApplicantID" or they are empty rows
+# remove extra rows 
+raw <- raw[ !is.na(raw$Applicant_ApplicantID), ]
 
-raw <- raw[!is.na(raw$Applicant_ApplicantID), ]
+raw$rand <- runif(nrow(raw), min = 1, max = 10000)
 
-rand <- runif(nrow(raw), min = 1, max = 1000000)
+# Some checking 
+stopifnot( sum( duplicated(raw$rand)) == 0)
+table(raw$Applicant_HasSiblingInSchool)
+table(raw$Applicant_CurrentDistrict)
+table(raw$Grade_GradeLevelCode)
+
+raw$priority.level <- with(raw,
+                           ifelse(Applicant_HasSiblingInSchool == "TRUE", 1,
+                           ifelse(Applicant_CurrentDistrict == "BPS", 2, 3)))
+
+raw <- raw %>% 
+  arrange(rand) %>%
+  mutate(random.lot = row_number())
+
+out <- raw %>%
+  group_by( Grade_GradeLevelCode) %>% 
+  arrange(priority.level, random.lot) %>%
+  mutate(priority.number = row_number(),
+         name = ifelse(UserCheck4 == "TRUE", "",Applicant_FullName)) 
 
 
+out.small <- out %>%
+  select(Applicant_ApplicantID, name, priority.number, priority.level, Grade_GradeLevelCode, random.lot)
+
+
+write.csv( out, "UAD Results/UP Dorchester Lottery Full Results March 11 2015.csv", row.names = F, na = "")
+write.csv( out.small, "UAD Results/UP Dorchester Lottery Presentation Results March 11 2015.csv", row.names = F, na = "")
