4b96e326eca4368533c03d6b2f653e796bd46f38
rgknight <ryangknight@gmail.com>
2015-04-24 14:17:14 -0400

    Initial Edits for Lottery 2

---
+++ Run Lottery 2 for UP Academy Boston.R
@@ -0,0 +1,64 @@
+# Generate a random sample and apply lottery logic for UP Boston
+
+# Where the information is saved
+# Is an application
+  # Every application will have a helper
+  # No rows that are not application will have a helper
+
+# Priority Level
+  # UAB is calculating the priority level in excel here - "Priority number"
+# Source data
+  # Level 1: Sibling - 'UP ID of enrolled sibling' is not anything close to missing
+  # Level 2: BPS student - 'Current school' is a BPS school
+  # Level 3: Everyone else
+
+# Process
+# (1) Generate a random number 
+# (2) Generate an order by priority level and grade
+
+options(stringsAsFactors=F)
+require(dplyr)
+
+setwd("C:/Dropbox (UP)/UP-Data Evaluation/UP Data Sources/School Data Requests/Lotteries 2015")
+
+raw <- read.csv("UAB Source/2015-2016 UAB Lottery 2.csv", strip.white = TRUE)
+
+set.seed(723462) # Change to a random number from random.org on lottery night
+
+# remove extra rows (everyone has a helper)
+raw <- raw[grepl("^NO", raw$Helper), ]
+
+raw$rand <- runif(nrow(raw), min = 1, max = 10000)
+
+# Assert that there are no duplicate random numbers and everyone has a priority status
+stopifnot( sum( duplicated(raw$rand)) == 0)
+tryCatch( nrow( table( raw$Priority.number)) == 3)
+
+raw <- raw %>% 
+  arrange(rand) %>%
+  mutate(random.lot = row_number())
+
+out <- raw %>%
+  group_by( X2015.2016.Grade) %>% 
+  arrange(Priority.number, random.lot) %>%
+  mutate(priority = row_number(),
+          name = ifelse(tolower(Do.Not.Read.at.Lottery) == "x", "",
+                        paste(First.name, Middle.name, Last.name, sep = " ") )) %>%
+  rename(priority.number = priority, priority.level = Priority.number)
+
+
+out.small <- out %>%
+  select(UP.ID, name, priority.number, priority.level, X2015.2016.Grade, random.lot)
+
+
+write.csv( out, "UAB Results/UAB Lottery 2 Results April 29 2015.csv", row.names = F, na = "")
+write.csv( out.small, "UAB Results/UAB Lottery 2 Results April 29 2015.csv", row.names = F, na = "")
+
+# Output small file == 
+#   Grade,
+# Priority Level,
+# Priority Order [order within grade], 
+# Applicant ID,
+# Name if not confidential,
+# random lot [overall random number]
+# Output Large file = everything
---
+++ Run Lottery 2 for UP Academy Dorchester.R
@@ -0,0 +1,59 @@
+# Generate a random sample and apply lotter logic for UAD
+
+# Process
+# (1) Generate a random number 
+# (2) Generate an order by priority level and grade
+
+
+# Where is this information saved?
+  # Is an application = Applicant_ApplicantID is not missing
+  # Grade = Grade_GradeLevelCode
+  # Priority Order
+    # Level 1: Sibling -- UserCheck1 == "TRUE"
+    # Level 2: Current BPS Student -- UserCheck2 == "TRUE"
+    # Level 3: Everyone else (should also be reflected by UserCheck3=="TRUE" )
+
+
+options(stringsAsFactors=F)
+require(dplyr)
+
+setwd("C:/Dropbox (UP)/UP-Data Evaluation/UP Data Sources/School Data Requests/Lotteries 2015")
+
+raw <- read.csv("UAD Source/Final 2015-2016 Lottery 2 (All Fields).csv")
+
+set.seed(13558837) # Change to a random number from random.org on lottery night
+
+# remove extra rows 
+raw <- raw[ !is.na(raw$Applicant_ApplicantID), ]
+
+raw$rand <- runif(nrow(raw), min = 1, max = 10000)
+
+# Some checking 
+stopifnot( sum( duplicated(raw$rand)) == 0)
+table(raw$UserCheck1)
+table(raw$UserCheck2)
+table(raw$Grade_GradeLevelCode)
+
+raw$priority.level <- with(raw,
+                           ifelse(UserCheck1 == "TRUE", 1,
+                           ifelse(UserCheck2 == "TRUE", 2, 3)))
+
+table(raw$priority.level)
+
+raw <- raw %>% 
+  arrange(rand) %>%
+  mutate(random.lot = row_number())
+
+out <- raw %>%
+  group_by( Grade_GradeLevelCode) %>% 
+  arrange(priority.level, random.lot) %>%
+  mutate(priority.number = row_number(),
+         name = ifelse(UserCheck4 == "TRUE", "",Applicant_FullName)) 
+
+
+out.small <- out %>%
+  select(Applicant_ApplicantID, name, priority.number, priority.level, Grade_GradeLevelCode, random.lot)
+
+
+write.csv( out, "UAD Results/UP Dorchester Lottery 2 Results April 29 2015.csv", row.names = F, na = "")
+write.csv( out.small, "UAD Results/UP Dorchester Lottery 2 Presentation Results April 29 2015.csv", row.names = F, na = "")
